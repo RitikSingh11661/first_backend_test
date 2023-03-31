1fd683b4817a5f2d2ab3c0765a1b41aa5ba4f40f
Ritik Singh <delhi8260@gmail.com>
2023-03-31 19:09:16 +0530

    change4

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,17 +2,19 @@ const jwt = require('jsonwebtoken');
 
 const auth=(req,res,next) => {
     const token = req.headers.authorization;
+    console.log('token',token)
     if(token){
         try {
         const decoded = jwt.verify(token,'user');
+        console.log('decoded',decoded)
         if(decoded){
             req.body.userID=decoded.userID;
             next();
         }
         } catch (error) {
-            res.status(400).send({"err":"You are not authorized"})
+            res.status(400).send({"err":"You are not authorized "})
         }
-    }else res.status(400).send({"err":"You are not authorized"})
+    }else res.status(400).send({"err":"You are not authorized 2"})
    
 }
 
